0796d315529f3c909edea02c7d2244d8d8057ff8
kshitijsubedi <2subedikshitij@gmail.com>
2021-05-25 17:41:21 +0545

    🔉 Audio added

---
+++ js/audio.js
@@ -0,0 +1,9 @@
+const backgroundSound = new Audio('./assets/sounds/main.mp3'),
+  star1Sound = new Audio('./assets/sounds/star_1.mp3'),
+  star2Sound = new Audio('./assets/sounds/star_2.mp3'),
+  star3Sound = new Audio('./assets/sounds/star_3.mp3'),
+  sadSound = new Audio('./assets/sounds/sad.mp3'),
+  eatSound = new Audio('./assets/sounds/eat.mp3'),
+  clickSound = new Audio('./assets/sounds/click.mp3');
+
+  backgroundSound.loop=true;
