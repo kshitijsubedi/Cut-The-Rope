98c9f11d099e38844ab83bce3b0606ef3267de32
kshitijsubedi <2subedikshitij@gmail.com>
2021-05-27 23:25:16 +0545

    🔥 Restart Button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-window.onload = function () {
-    let game = new Game(levels[0])
-};
+let restartButton = document.getElementById('restart-button')
+let game;
+let startLevel = (levelIndex) => {
+    restartButton.style.display='block'
+    currentLevel=levelIndex
+    game = new Game(levels[levelIndex])
+}
+
+restartButton.addEventListener('click',()=>{
+    window.cancelAnimationFrame(game.animationFrame);
+   startLevel(currentLevel)
+})
