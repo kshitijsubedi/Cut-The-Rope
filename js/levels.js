db7e08bc01f511096f62a20f15b8e5ab494253ed
kshitijsubedi <2subedikshitij@gmail.com>
2021-05-28 14:49:04 +0545

    🌱  one more level

--- js/levels.js
+++ js/levels.js
@@ -45,16 +45,16 @@ levels.push({
 });
 
 levels.push({
-    candy: { x: width / 3, y: height / 3 },
+    candy: { x: width / 2, y: height / 2 },
     pinPoints :[
-        { x: width / 3, y: height / 5 ,length:15},
-        { x: width / 2.2, y: height / 5,length:15},
-        { x: width / 1.5, y: height / 5 ,length:25}
+        { x: width / 2, y: height / 5 ,length:15},
+        { x: width / 3, y: height / 2,length:13},
+        { x: width / 1.5, y: height / 2 ,length:13}
     ],
     stars: [
         new Star({ x: width / 2.5, y: height / 2 }, 1),
-        new Star({ x: width / 2, y: height / 1.7 }, 6),
-        new Star({ x: width / 2.5, y: height / 1.4 }, 10),
+        new Star({ x: width / 2.5, y: height / 1.4 }, 6),
+        new Star({ x: width / 2, y: height / 1.4 }, 10),
     ],
     frog: new Frog({ x: width / 2, y: height - 120 }),
 });
