cf4c77dbae1ffa7ec5d0db698b0e777d098f859e
kshitijsubedi <2subedikshitij@gmail.com>
2021-05-28 15:40:50 +0545

    ⚡️ More

--- js/levels.js
+++ js/levels.js
@@ -74,3 +74,18 @@ levels.push({
     ],
     frog: new Frog({ x: width / 2, y: height - 120 }),
 });
+
+levels.push({
+    candy: { x: width / 2, y: height / 2 },
+    pinPoints :[
+        { x: width / 2.5, y: height / 2 ,length:10},
+        { x: width / 2.2, y: height / 1.5,length:10},
+        { x: width / 1.5, y: height / 3 ,length:15}
+    ],
+    stars: [
+        new Star({ x: width / 1.8, y: height / 1.4 }, 1),
+        new Star({ x: width / 2, y: height / 1.7 }, 6),
+        new Star({ x: width / 2.5, y: height / 1.4 }, 10),
+    ],
+    frog: new Frog({ x: width / 2, y: height - 120 }),
+});
